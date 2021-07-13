3460a2724ff10a03c97fe17df633fa792d8b70e3
parth7498 <parthvijay74@gmail.com>
2021-07-13 18:25:17 +0530

    mail file

---
+++ mail.py
@@ -0,0 +1,37 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.base import MIMEBase
+from email import encoders
+import os
+import time
+
+# time.sleep(6)
+mail_content = "Hello, This is a test mail."
+
+#The mail addresses and password
+sender_address = <senders-mail>
+sender_pass = <senders-password>
+receiver_address = <receivers-mail>
+#Setup the MIME
+message = MIMEMultipart()
+message['From'] = sender_address
+message['To'] = receiver_address
+message['Subject'] = 'Warning'
+
+## Send Text in E-Mail:
+message.attach(MIMEText(mail_content, 'plain'))
+
+## Send Image in E-Mail:
+fp = open("/home/parth/Pictures/pendrive/image.png", 'rb')
+message.attach(MIMEImage(fp.read()))
+fp.close()
+
+session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+session.starttls() #enable security
+session.login(sender_address, sender_pass) #login with mail_id and password
+text = message.as_string()
+session.sendmail(sender_address, receiver_address, text)
+session.quit()
+print('Mail Sent')
